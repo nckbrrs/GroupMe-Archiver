0c3c8c41c7b078c9a730d014bc962fd228a0aca4
Nick Barrs <nickbarrs@115.182.21.198.tigernet.wifi.dyn.clemson.edu>
2017-09-20 14:57:01 -0400

    updated get_group_ids

--- get_group_ids.py
+++ get_group_ids.py
@@ -5,24 +5,23 @@ import json
 
 def get_groups(token, outfile):
     groups_endpoint = 'https://api.groupme.com/v3/groups?token=' + token
-    groups_response = requests.get(endpoint)
+    groups_response = requests.get(groups_endpoint)
 
-    if response.status_code is not 200:
-        print response.status_code
-        print response.headers
-        print response.text
+    if groups_response.status_code is not 200:
+        print "\nHTTP error code " + str(groups_response.status_code) + "\n"
         sys.exit(2)
 
     groups_json = groups_response.json()
+    outfile.write("GroupMe groups in which you are a member, and their IDs:\n\n")
     for group in groups_json['response']:
         outfile.write('Group Name: ' + group['name'] + '\n')
         outfile.write('Group ID: ' + group['id'] + '\n\n')
 
 def main():
     if len(sys.argv) != 2:
-        print "\nPlease execute using the following example format:\n" \
-              "             argv[0]               argv[1]\n" \
-              "$ python get_group_ids.py YOUR_ACCESS_TOKEN\n"
+        print "\nPlease execute using the following example format:" \
+              "\n             argv[0]               argv[1]" \
+              "\n$ python get_group_ids.py YOUR_ACCESS_TOKEN\n"
         sys.exit(1)
 
     token = sys.argv[1]
@@ -30,8 +29,8 @@ def main():
     get_groups(token, groups_file)
     groups_file.close()
     print "\nA text file, 'my_group_ids.txt' has been created that holds" \
-          "the names of the groups that you are in as well as their" \
-          "associated unique Group IDs!\n"
+          "\nthe names of the groups that you are in as well as their" \
+          "\nassociated unique Group IDs!\n"
 
 if __name__ == '__main__':
     main()
